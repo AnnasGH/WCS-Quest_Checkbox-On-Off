8727051c7b60453d890733a218ac11f1ac5e6320
AnnasGH <48451437+AnnasGH@users.noreply.github.com>
2021-11-10 21:55:13 +0100

    update on checkbox

--- src/components/Checkbox.js
+++ src/components/Checkbox.js
@@ -1,8 +1,14 @@
 import React, { Component } from "react";
 
 function Checkbox(props) {
-  const { active, handleChange } = props;
-  return <input type="checkbox" onChange={handleChange} checked={active} />;
+  /*const { active, handleChange } = props;*/
+  return (
+    <input
+      type="checkbox"
+      onChange={props.handleChange}
+      checked={props.active}
+    />
+  );
 }
 
 export default Checkbox;
